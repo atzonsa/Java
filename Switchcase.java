c77e24ba549faff50e0a04e123882def08e40094
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-02-28 21:32:50 +0530

    Switch Case

---
+++ Switchcase.java
@@ -0,0 +1,23 @@
+package myfirstproject;
+
+public class Switchcase {
+public static void main(String[]arg){
+    int input=2;
+    switch(input){
+        case 1:
+            System.out.println("this is case 1");
+            break;
+        case 2:
+           System.out.println("this is case 2"); 
+           break;
+        case 3:
+           System.out.println("this is case 3");
+           break;
+        case 4:
+           System.out.println("this is case 4");
+        break;
+        default:
+            System.out.println("Invalid input");
+    }
+}    
+}
