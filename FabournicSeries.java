99a816df0c51f56e6c8069c8b6f0dc4f8f4bca07
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-02-29 19:57:31 +0530

    fabournic series

--- FabournicSeries.java
+++ FabournicSeries.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package myfirstproject;
 
 import java.util.Scanner;
 
-/**
- *
- * @author RANGA
- */
 public class FabournicSeries {
     public static void main(String[]arg){
         while (true)
