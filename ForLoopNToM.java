37b9ec42a1f1331f8942de8ce2070cf8a117b61d
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-02-29 19:57:42 +0530

    forloop n to m

--- ForLoopNToM.java
+++ ForLoopNToM.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package myfirstproject;
 import java.util.Scanner;
-/**
- *
- * @author RANGA
- */
+
 public class ForLoopNToM {
     public static void main(String[]arg){
         while (true)
