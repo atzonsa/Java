f41aa22808373f928949e80855eb9ef9ea7c83f5
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-01 22:09:46 +0530

    Conditional

---
+++ Conditional.java
@@ -0,0 +1,14 @@
+package myfirstproject;
+
+public class Conditional {
+    public static void main(String[]arg){
+        int a=450,b=465;
+        if(a>b){
+            System.out.println(a);  
+        }
+        else if(b>a)
+            System.out.println(b);
+        else
+            System.out.println("Both are equals");
+     }   
+}
