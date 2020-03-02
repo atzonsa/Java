9922578649633652f270b06dc9d30b0f0541f7bb
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-02 21:33:40 +0530

    for loop

---
+++ Forloop.java
@@ -0,0 +1,12 @@
+package myfirstproject;
+
+public class Forloop {
+    public static void main(String[]arg){
+        int sum=0;
+        for(int i=1;i<=100;i++)
+        {
+            sum+=i;
+        }
+        System.out.println(sum);
+    }    
+}
