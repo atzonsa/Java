99a2a89a8d884e43216ea27e3d17c47262722a49
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-03 21:03:55 +0530

    function in java

---
+++ Functioninjava.java
@@ -0,0 +1,15 @@
+package myfirstproject;
+
+public class Functioninjava {
+//function is a block of code which is used to separate the module of the program
+//we can reuse the function 
+//return_type function_name(args){statements} 
+    //number ==>binary number
+    void display(){
+    System.out.println("Function is working");
+    }
+    public static void main(String arg[]){
+    Functioninjava obj=new Functioninjava();
+    obj.display();obj.display();obj.display();obj.display();
+    }
+}
